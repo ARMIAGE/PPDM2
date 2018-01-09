7bec4138c0de650fd4788187742674f6aef99052
Seefy <alex.veynand@yahoo.fr>
2018-01-09 23:42:40 +0100

    Fonction insertion pour fichier xlsx cos matrix

--- src/f_insert_file_cos_matrix_brm_IFR.py
+++ src/f_insert_file_cos_matrix_brm_IFR.py
@@ -2,14 +2,14 @@ import pandas as pd
 import xlrd
 
 
-def insert_file_cos_matrix_brm_ifr(path):
+def f_insert_file_rg(path):
 
-    # recuperation des donnees de chaque onglet
-    first_sheet = pd.read_excel(path, sheetname='1st_200')
-    second_sheet = pd.read_excel(path, sheetname='2nd_200')
-    third_sheet = pd.read_excel(path, sheetname='last_141')
+    # récupération des données de chaque onglet
+    first_sheet = pd.read_excel(path,sheetname='1st_200')
+    second_sheet = pd.read_excel(path,sheetname='2nd_200')
+    third_sheet = pd.read_excel(path,sheetname='last_141')
     
-    # depivotage des donnees de chaque onglet du fichier excel pour avoir la structure : mot 1, mot 2, correlation
+    # dépivotage des données de chaque onglet du fichier excel pour avoir la structure : mot 1, mot 2, corrélation
     unpivot_first_sheet = pd.melt(first_sheet, id_vars=['CONCEPT'], var_name="mot 2", value_name="correlation")
     unpivot_second_sheet = pd.melt(second_sheet, id_vars=['CONCEPT'], var_name="mot 2", value_name="correlation")
     unpivot_third_sheet = pd.melt(third_sheet, id_vars=['CONCEPT'], var_name="mot 2", value_name="correlation")
@@ -19,6 +19,6 @@ def insert_file_cos_matrix_brm_ifr(path):
     
     matrix = dataframe.as_matrix()
     
-    return matrix
+    return (matrix)
 
-
+f_insert_file_rg(r'''C:\Users\alexv\Documents\MIAGE\MASTER\Master2\PPD\Dataset\cos_matrix_brm_IFR.xlsx''')
