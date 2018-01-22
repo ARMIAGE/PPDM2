7e5ec9440f1ea03584a00c86037f3381957d5206
Alexis Colonna <alexdu91390@yahoo.fr>
2018-01-22 09:23:49 +0100

    Refactoring insertion fichier

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from f_insert_file_simlex999 import insert_file_simlex999
+from file_insertion import insert_file_simlex999
 from f_insert_file_rel122 import insert_file_rel122
 from f_insert_file_mturk771 import insert_file_mturk771
 from f_insert_file_mc import insert_file_mc
