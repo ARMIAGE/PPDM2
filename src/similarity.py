8263772d9b5f6018399218e48056e68f548b958a
Alexis Colonna <alexdu91390@yahoo.fr>
2018-01-22 11:03:05 +0100

    correction similarité doublons git

--- src/similarity.py
+++ src/similarity.py
@@ -7,27 +7,7 @@ def similarite(matrice):
 
     # Creation matrice W2V (mot1, mot2, coef)
     matrice_word2vec = calcul_similarite(liste_mots)
-
-    #Tri par ordre de similarité
-    matrice = tri_matrice(matrice)
-    matrice_word2vec = tri_matrice(matrice_word2vec)
     
-    #Ajout du rang
-    matrice = ajouter_colonne_rang(matrice) 
-    matrice_word2vec = ajouter_colonne_rang(matrice_word2vec)
-    
-    #Tri Alphabetique pour comparaison finale
-    matrice = tri_alphabetique(matrice)
-    matrice_word2vec = tri_alphabetique(matrice)
-    
-    #Recuperation du rang
-    rang_humain = get_rang(matrice)
-    rang_word2vec = get_rang(matrice_word2vec)
-    
-    #Qualité du fichier comparé
-    coef_similarite = calcul_corellation(rang_humain, rang_word2vec) #KENDALL
-return coef_similarite
-
     # Tri par ordre de similarite
     matrice = fonctions_utiles.tri_matrice(matrice)
     matrice_word2vec = fonctions_utiles.tri_matrice(matrice_word2vec)
