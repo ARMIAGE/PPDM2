30acf144dfb373b44af03b5bd3788bfec5293d4e
ARMIAGE <alexandre.rosselin@gmail.com>
2018-03-06 10:01:32 +0100

    Correlation done

--- src/main.py
+++ src/main.py
@@ -2,12 +2,17 @@ from gensim.models import word2vec
 import file_insertion
 import similarity
 import fonctions_utiles
+import warnings
 
+warnings.filterwarnings('ignore', '.*nan.*',)
+
+    
 model = word2vec.Word2Vec.load('../MODEL/W2V_text8_Model.bin')
 
-CSVFile = "../DATA/SimLex-999.txt"
-data = file_insertion.insert_file_simlex999(CSVFile)
+file = "../DATA/SimLex-999.txt"
+data = file_insertion.insert_file_simlex999(file)
 
-matrice = similarity.similarite(model, data)
-
-print(matrice)
+coef = similarity.similarite(model, data)
+coef = round(coef*100)
+print("Le coefficient de qualité du fichier (%)")
+print(coef)
